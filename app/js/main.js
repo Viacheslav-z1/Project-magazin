bd0808cb062c4ad76b4c5e14b71389ddf3dc3fcb
Viacheslav-z1 <slavik.zaliotnyi@gmail.com>
2022-04-09 13:16:08 +0300

    added product1, and incleded mixitup in product1

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,14 @@ $(function () {
     autoplaySpeed: 3500,
     fade: true
   })
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 })

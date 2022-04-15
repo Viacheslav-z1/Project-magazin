0d1249734be902a48a884f3cdd5355839b1de393
Viacheslav-z1 <slavik.zaliotnyi@gmail.com>
2022-04-15 15:08:44 +0300

    added product__list-1

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,29 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     fade: true
-  })
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  $('.filter-recent__stars').rateYo({
+    starWidth: "11px",
+    readOnly: true,
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "8px",
+    
+  });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
   var containerEl2 = document.querySelector('[data-ref="container-2"]');
@@ -16,4 +38,5 @@ $(function () {
   };
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
+
 })

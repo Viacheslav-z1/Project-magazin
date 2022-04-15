3ee8431580de0033937e028bf37884046ca7240c
Viacheslav-z1 <slavik.zaliotnyi@gmail.com>
2022-04-15 17:38:27 +0300

    added product__list-2 page

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,26 @@ $(function () {
     },
   });
 
+  // $('.product-2__add-to-cart').on('click',function () {
+  //   $('.product-2__added-to_cart').toggleClass('active');
+  //   $('.product-2__add-to-cart').toggleClass('active');
+  // });
+
+  // $('.product-2__cart-btn', this).on('click', function () {
+  //   $('.product-2__added-to_cart').removeClass('active');
+  //   $('.product-2__add-to-cart').removeClass('active');
+  // });
+
+  $(this).find('.product-2__add-to-cart').on('click', function () {
+    $('.product-2__added-to_cart').toggleClass('active');
+    $('.product-2__add-to-cart').toggleClass('active');
+  });
+
+  $(this).find('.product-2__cart-btn').on('click', function () {
+    $('.product-2__added-to_cart').removeClass('active');
+    $('.product-2__add-to-cart').removeClass('active');
+  });
+
   $('.filter-recent__stars').rateYo({
     starWidth: "11px",
     readOnly: true,
@@ -27,6 +47,14 @@ $(function () {
     ratedFill: "#ffcc00",
     spacing: "8px",
     
+  });
+
+  $('.product-2__star').rateYo({
+    starWidth: "20px",
+    readOnly: true,
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "12px",
   });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
